be7e104d1db7af00d7fc62f88ecbbc32c518007a
Diogo Leite <diogo1790@hotmail.com>
2018-10-23 13:27:35 +0200

    family management #5

--- objects_API/FamilyJ.py
+++ objects_API/FamilyJ.py
@@ -4,6 +4,11 @@ from rest_client.FamilyRest import FamilyAPI
 
 
 class FamilySchema(Schema):
+    """
+    This class map the json into the object Family
+
+    ..note:: see marshmallow API
+    """
     id = fields.Int()
     designation = fields.Str()
     genuses = fields.List(fields.Url)
@@ -13,21 +18,59 @@ class FamilySchema(Schema):
         return FamilyJson(**data)
 
 class FamilyJson(object):
+    """
+    This class manage the object and is used to map them into json format
+    """
 
+    def __init__(self, id = None, designation = '', genuses=[]):
+        """
+        Initialization of the class
 
-    def __init__(self, id = None, designation = '', genuses=()):
+        :param id: name of the function
+        :param designation: name of the family
+        :param genuses: vector of genus endpoints
+
+        :type id: int
+        :type designation: string 
+        :type genuses: vector[string]
+
+        """
         self.id = id
         self.designation = designation
         self.genuses = genuses
 
     def __str__(self):
+        """
+        override the Str function 
+
+        """
         return 'id: {0} designation {1}'.format(self.id, self.designation)
 
 
 
-    def getAllAPI(self):
+    def getAllAPI():
+
+        """
+        get all the families on the database
+
+        :return: list of Families
+        :rtype: vector[FamilyJ]
+        """
         list_family = FamilyAPI().get_all()
         schema = FamilySchema()
         results = schema.load(list_family, many=True)
+        return results[0]
 
+    def setFamily(self):
+        """
+        set new family
+
+        :return: new family completed with the id
+        :rtype: FamilyJ
+        """
+        schema = FamilySchema(only=['designation'])
+        jsonFam = schema.dump(self)
+        resultsCreation = FamilyAPI().set_family(jsonData = jsonFam.data)
+        schema = FamilySchema()
+        results = schema.load(resultsCreation)
         return results[0]
