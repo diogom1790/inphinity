bf93d76f2537997cf3df5b72a2dc428c6353eafc
Diogo Leite <diogo1790@hotmail.com>
2018-10-23 13:55:47 +0200

    genus queries class #6

---
+++ rest_client/GenusRest.py
@@ -0,0 +1,43 @@
+import json
+from rest_client.GetRest import GetRest
+from rest_client.PostRest import PostRest
+
+class GenusAPI(object):
+    """
+    This class manage the requests for the genus objects into the restAPI
+
+    :param function: the name of the function to access in the rest API
+    :type function: string
+    """
+
+    def __init__(self, function='genus/'):
+        """
+        Initialization of the class
+
+        :param function: name of the function
+
+        :type function: string (url)
+
+        """
+        self.function = function
+
+    def get_all(self):
+        """
+        get all the genus on the database
+
+        :return: json file with all the data
+        :rtype: string (json format)
+        """
+        result_get = GetRest(function = self.function).performRequest()
+        return result_get
+
+    def set_genus(self, jsonData):
+        """
+        set new genus in the database
+
+        :return: json file with the last genus created
+        :rtype: string (json format)
+        """
+        jsonData = json.dumps(jsonData)
+        result_post = PostRest(function = self.function, dataDict = jsonData).performRequest()
+        return result_post
