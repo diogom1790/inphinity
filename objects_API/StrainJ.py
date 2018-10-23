d4f78dd66f8ac8bfed75a32349cb941199361a65
Diogo Leite <diogo1790@hotmail.com>
2018-10-23 14:34:50 +0200

    Strain management #5

---
+++ objects_API/StrainJ.py
@@ -0,0 +1,77 @@
+from marshmallow import Schema, fields, post_load
+
+from rest_client.StrainRest import StrainAPI
+
+class StrainSchema(Schema):
+    """
+    This class map the json into the object Strain
+
+    ..note:: see marshmallow API
+    """
+    id = fields.Int()
+    designation = fields.Str()
+    specie = fields.Int()
+    bacteria = fields.List(fields.Url)
+
+    @post_load
+    def make_Strain(self, data):
+        return StrainJson(**data)
+
+class StrainJson(object):
+    """
+    This class manage the object and is used to map them into json format
+    """
+
+    def __init__(self, id = None, designation = '', specie = None, bacteria=[]):
+        """
+        Initialization of the class
+
+        :param id: name of the function
+        :param designation: name of the strain
+        :param specie: id of the specie
+        :param bacteria: vector of species endpoints
+
+        :type id: int
+        :type designation: string 
+        :type specie: int 
+        :type bacteria: vector[string]
+
+        """
+        self.id = id
+        self.designation = designation
+        self.specie = specie
+        self.bacteria = bacteria
+
+    def __str__(self):
+        """
+        override the Str function 
+
+        """
+        return 'id: {0} designation {1} FK specie {2} Nb bacteria {3}'.format(self.id, self.designation, self.specie, len(self.bacteria))
+
+    def getAllAPI():
+
+        """
+        get all the strains on the database
+
+        :return: list of Strain
+        :rtype: vector[StrainJ]
+        """
+        list_strain = StrainAPI().get_all()
+        schema = StrainSchema()
+        results = schema.load(list_strain, many=True)
+        return results[0]
+
+    def setStrain(self):
+        """
+        set new strain
+
+        :return: new strain completed with the id
+        :rtype: StrainJ
+        """
+        schema = StrainSchema(only=['designation', 'specie'])
+        jsonFam = schema.dump(self)
+        resultsCreation = StrainAPI().set_strain(jsonData = jsonFam.data)
+        schema = StrainSchema()
+        results = schema.load(resultsCreation)
+        return results[0]
