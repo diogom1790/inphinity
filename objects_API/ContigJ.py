6e65d1b074fb036f94482d978f7d72df262ec6fd
Diogo Leite <diogo1790@hotmail.com>
2018-10-26 14:43:53 +0200

    Add management for the contig #6

---
+++ objects_API/ContigJ.py
@@ -0,0 +1,86 @@
+from marshmallow import Schema, fields, post_load
+
+from rest_client.ContigRest import ContigAPI
+
+class ContigSchema(Schema):
+    """
+    This class map the json into the object WholeDNA
+
+    ..note:: see marshmallow API
+    """
+    id = fields.Int()
+    id_db_online = fields.Str()
+    sequence_DNA = fields.Str()
+    fasta_head = fields.Str()
+    organism = fields.Int()
+    protein_contig = fields.List(fields.Int)
+
+    @post_load
+    def make_Contig(self, data):
+        return ContigJson(**data)
+
+class ContigJson(object):
+    """
+    This class manage the object and is used to map them into json format
+    """
+
+    def __init__(self, id = None, id_db_online = None, sequence_DNA= None, fasta_head = None, organism= None, protein_contig = None):
+        """
+        Initialization of the class
+
+        :param id: name of the function
+        :param id_db_online: id of the database where the dna comes
+        :param sequence_DNA: DNA sequence
+        :param fasta_head: head of the fasta from where the DNA was extracted
+        :param organism: id of the organism
+        :param protein_contig: list of the proteins in the contig
+
+        :type id: int
+        :type id_db_online: string 
+        :type sequence_DNA: string
+        :type fasta_head: string
+        :type organism: int
+        :type protein_contig: List(int)
+
+        """
+        self.id = id
+        self.id_db_online = id_db_online
+        self.sequence_DNA = sequence_DNA
+        self.fasta_head = fasta_head
+        self.organism = organism
+        self.protein_contig = protein_contig
+
+    def __str__(self):
+        """
+        override the Str function 
+
+        """
+        return 'id: {0} fasta head {2}'.format(self.id, self.fasta_head)
+
+    def getAllAPI():
+
+        """
+        get all the Contig on the database
+
+        :return: list of Contig
+        :rtype: vector[ContigJson]
+        """
+        list_contigs = ContigAPI().get_all()
+        schema = ContigSchema()
+        results = schema.load(list_contigs, many=True)
+        return results[0]
+
+    def setContig(self):
+        """
+        set new contig
+
+        :return: new contig completed with the id
+        :rtype: ContigJson
+        """
+
+        schema = ContigSchema(only=['id_db_online','sequence_DNA','fasta_head','organism'])
+        jsonContig = schema.dump(self)
+        resultsCreation = ContigAPI().set_contig(jsonData = jsonContig.data)
+        schema = ContigSchema()
+        results = schema.load(resultsCreation)
+        return results[0]
---
+++ rest_client/ContigRest.py
@@ -0,0 +1,43 @@
+import json
+from rest_client.GetRest import GetRest
+from rest_client.PostRest import PostRest
+
+class ContigAPI(object):
+    """
+    This class manage the requests for the Contig objects into the restAPI
+
+    :param function: the name of the function to access in the rest API
+    :type function: string
+    """
+
+    def __init__(self, function='contig/'):
+        """
+        Initialization of the class
+
+        :param function: name of the function
+
+        :type function: string (url)
+
+        """
+        self.function = function
+
+    def get_all(self):
+        """
+        get all the Contigs on the database
+
+        :return: json file with all the data
+        :rtype: string (json format)
+        """
+        result_get = GetRest(function = self.function).performRequest()
+        return result_get
+
+    def set_contig(self, jsonData):
+        """
+        set new contig in the database
+
+        :return: json file with the last contig created
+        :rtype: string (json format)
+        """
+        jsonData = json.dumps(jsonData)
+        result_post = PostRest(function = self.function, dataDict = jsonData).performRequest()
+        return result_post
