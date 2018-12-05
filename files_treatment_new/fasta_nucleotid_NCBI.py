c391c772c7feaa3e7aca67939c6b3036b18fa783
Diogo Leite <diogo1790@hotmail.com>
2018-12-05 11:33:51 +0100

    Add file to read nucleotid fasta file from NCBI #11

--- files_treatment_new/fasta_nucleotid_NCBI.py
+++ files_treatment_new/fasta_nucleotid_NCBI.py
@@ -16,21 +16,29 @@ class Fasta_nucleotid_NCBI (_generic_fasta_file):
 
     """
 
-    def __init__(self, path_file):
+    def __init__(self, path_file, identification_key):
         """
-        Constructor of the class fasta protein class, this one contain all methods for the treatment of fasta protein - nucleic sequence of NCBI platform. After the parameters initialisation, the datails loaded
+        Constructor of the class fasta protein class, this one contain all methods for the treatment of fasta protein - nucleic sequence of NCBI platform. After the parameters initialisation, the details loaded
 
         :param path_file: Complete path with file name
+        :param identification_key: Key used to obtain the different proteins in the fasta files
 
         :type path_file: string - required
+        :type identification_key: string - required
         """
         _generic_fasta_file.__init__(self, path_file)
         self.read_fasta_file()
+        self.identification_key = identification_key
+
 
 
     def get_list_nucleotid_name(self):
         """
-        This method return a list of the proteins_id in the fasta file
+        This method return a list of the key_designation in the fasta file
+
+        :param key_designation: Key used to obtain the information in the fasta file
+
+        :type key_designation: string - required
 
         :return: list of id proteins
         :rtype: list(string)
@@ -38,10 +46,12 @@ class Fasta_nucleotid_NCBI (_generic_fasta_file):
         list_id = []
         for key, value in self.dict_fasta_data.items():
             dict_elements = self.split_header_by_square_brackets(value.description)
-            list_id.append(dict_elements['protein_id'])
+
+            list_id.append(dict_elements[self.identification_key])
         return list_id
 
-    def get_nucleotid_sequence_prot_id(self, protein_id):
+
+    def get_nucleotid_sequence_by_key(self, protein_id):
         """
         This method return the nucleotid sequence based on the protein_id (ACC)
 
@@ -50,7 +60,7 @@ class Fasta_nucleotid_NCBI (_generic_fasta_file):
         """
         for key, value in self.dict_fasta_data.items():
             dict_elements = self.split_header_by_square_brackets(value.description)
-            if dict_elements['protein_id'] == protein_id:
+            if dict_elements[self.identification_key] == protein_id:
                 return str(value.seq)
         return ''
 
@@ -63,7 +73,7 @@ class Fasta_nucleotid_NCBI (_generic_fasta_file):
         """
         for key, value in self.dict_fasta_data.items():
             dict_elements = self.split_header_by_square_brackets(value.description)
-            if dict_elements['protein_id'] == protein_id:
+            if dict_elements[self.identification_key ] == protein_id:
                 location = dict_elements['location']
                 print(location)
                 search_loc = re.findall('\d+' ,location)
