6c5d18e8d8fa4a5a532a418db73778c517f00d37
Diogo Leite <diogo1790@hotmail.com>
2019-02-15 11:37:34 +0100

    comment the class that load the ids #22

--- Migration_organism_interactions_load_file.py
+++ Migration_organism_interactions_load_file.py
@@ -6,22 +6,66 @@ from configuration.configuration_api import ConfigurationAPI
 from rest_client.AuthenticationRest import AuthenticationAPI
 
 def get_data_interactions_by_csv(path_file):
+    """
+    load the interactions according the path file
+
+
+    :param path_file: file path name
+
+    :type path_file: str - required   
+
+    :return dataframe with the information
+    :rtype pandas dataframe
+    """
     dataframe_interaction = pd.read_csv(filepath_or_buffer = path_file)
     return dataframe_interaction
 
 
 def get_id_bacterium_by_acc(bacterium_acc):
+    """
+    get the id of a bacterium according to its acc number
+
+
+    :param bacterium_acc: acc number
+
+    :type path_file: str - required   
+
+    :return id of the bacterium
+    :rtype int
+    """
+
     bacterium_obj = BacteriumJson.getByAccnumber(bacterium_acc)
     bacterium_id = bacterium_obj.id
     return bacterium_id
 
 def get_id_bacteriophage_by_designation(phage_designation:str):
+    """
+    get the id of a bacteriophage according to its designation
+
+    :param phage_designation: designation of the bacteriophage
+
+    :type phage_designation: str - required   
+
+    :return id of the bacteriophage
+    :rtype int
+    """
     bacteriophage_obj = BacteriophageJson.getBydesignation(phage_designation)
     bacteriophage_id = bacteriophage_obj.id
     return bacteriophage_id
 
 
 def get_dictionary_index_bacterium(dataframe_interactions):
+    """
+    get a dictionary with the ids of the bacteria in the dataframe
+
+    :param dataframe_interactions: dataframe with all the data
+
+    :type dataframe_interactions: dataframe - required   
+
+    :return dictionary with the bacteriums ids
+    :rtype dict{[acc_value]:id}
+    """
+
     dataframe_data = dataframe_data.sort_values(by=['acc_bacterium'], ascending=[1])
     old_acc = ''
     dict_ids_bact = {}
@@ -38,6 +82,16 @@ def get_dictionary_index_bacterium(dataframe_interactions):
     return dict_ids_bact
 
 def getIdsBacteriophagesByDataframe(dataframe_interactions):
+    """
+    get a dictionary with the ids of the bacteriophages in the dataframe
+
+    :param dataframe_interactions: dataframe with all the data
+
+    :type dataframe_interactions: dataframe - required   
+
+    :return dictionary with the bacteriophage ids
+    :rtype dict{[acc_value]:id}
+    """
     dataframe_data = dataframe_data.sort_values(by=['phage_name'], ascending=[1])
     old_design = ''
     dict_designation_id = {}
@@ -59,7 +113,9 @@ AuthenticationAPI().createAutenthicationToken()
 path_csv_file = 'data_interactions.csv'
 
 dataframe_data = get_data_interactions_by_csv(path_csv_file)
-dataframe_data = dataframe_data.sort_values(by=['acc_bacterium'], ascending=[1])
+
+dict_ids_bacteria = get_dictionary_index_bacterium(dataframe_data)
+dict_ids_bacteriophage = getIdsBacteriophagesByDataframe(dataframe_data)
 
 
 
