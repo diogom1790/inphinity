6956cb84128f11dfffd7d2baf518f0ad813ee936
Diogo Leite <diogo1790@hotmail.com>
2019-02-20 10:55:55 +0100

    Update script that insert datagreg into db #23

--- migration_greg_new_interactions.py
+++ migration_greg_new_interactions.py
@@ -38,11 +38,13 @@ def getDictionaryFromCSV(path_csv:str):
 
 
 def addInteractionsNewDB(interaction_type:bool, bacterium_id:int, phage_id:int, level_id:int, lysis_id:int, persone_responsible:int, source_data_id:int, validity_id:bool):
-       
+    couple_obj_json = None 
     couple_obj = CoupleJson(interaction_type = interaction_type, bacteriophage = phage_id, bacterium = bacterium_id, level = level_id, lysis = lysis_id, person_responsible = persone_responsible, source_data = source_data_id, validity = validity_id)
 
-
-    couple_obj_json = couple_obj.setCoupleWithLysis()
+    if lysis_id != None:
+        couple_obj_json = couple_obj.setCoupleWithLysis()
+    else:
+        couple_obj_json = couple_obj.setCouple()
     return couple_obj_json
 
 
@@ -64,15 +66,21 @@ def getBacteriumStrainSpecieDesignationById(id_bacterium):
     return taxonomy_bacterium
 
 
-def validateDataRow(list_phages_names:list, bacterium_id:int, dict_phages_id:dict, row_dataframe):
+def validateDataRow(list_phages_names:list, bacterium_id:int, dict_phages_id:dict, dict_types_interactions_id:dict, row_dataframe):
     couple_interaction_type = True
+    type_lysis = None
     for phage_name in list_phages_names:
         phage_id = dict_phages_id[str(phage_name)]
         interaction_type = row_dataframe[phage_name]
         print(type(interaction_type))
-        if isinstance(interaction_type, int):
+        if is_int(interaction_type):
             couple_interaction_type = False
             print(couple_interaction_type)
+        elif interaction_type in dict_types_interactions_id:
+            type_lysis = dict_types_interactions_id[interaction_type]
+
+        addInteractionsNewDB(couple_interaction_type, bacterium_id, phage_id, 4, type_lysis, persone_responsible = 5, source_data_id = 3, validity_id = 1)
+
         print(phage_id)
     print('Hello')
 
@@ -101,6 +109,11 @@ dict_phages_id['77'] = 4942
 dict_phages_id['K'] = 4381
 dict_phages_id['Sb-1'] = 4457
 
+dict_types_interactions = {}
+dict_types_interactions['CL'] = 5
+dict_types_interactions['SCL'] = 6
+dict_types_interactions['OL'] = 7
+
 
 list_heads = list(dataframe_file_interaction.head(0))
 list_heads_phages = list_heads[1:]
@@ -111,6 +124,6 @@ for index, row in dataframe_file_interaction.iterrows():
     taxonomy_bacterium = getBacteriumStrainSpecieDesignationById(id_bacterium)
 
     #ask the user if we can insert the data
-    validateDataRow(list_heads_phages, id_bacterium, dict_phages_id, row)
+    validateDataRow(list_heads_phages, id_bacterium, dict_phages_id, dict_types_interactions, row)
     print(bacterium_name)
     #getIdOrganismContainName(dataframe_file_bacterium_list, bacterium_name)
