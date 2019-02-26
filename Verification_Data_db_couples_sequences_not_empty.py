b8fcdf733bde9649c5f12b93a06348e7f347a54f
diogom1790 <diogo.leite@heig-vd.ch>
2019-02-26 16:45:35 +0100

    add comments to the verification sequence data

--- Verification_Data_db_couples_sequences_not_empty.py
+++ Verification_Data_db_couples_sequences_not_empty.py
@@ -10,6 +10,56 @@ from objects_API.ContigJ import ContigJson
 
 
 
+def checkSequenceProteins(list_proteins:ProteinJson):
+    """
+    check if the sequence exists for a protein given a list of them
+
+    :param list_proteins: list of the proteins
+
+    :type id: array[ProteinJ]
+
+    :return: dictionary with the ids and sequences if they are to short or inexistent
+    :rtype: dict[id_prot]:sequence
+    """
+    dict_proteins_error = {}
+    for protein in list_proteins:
+        if len(protein.sequence_AA) < 15:
+            print('error')
+            id_prot = protein.id
+            sequence_prot = protein.sequence_AA
+            dict_proteins_error[id_prot] = sequence_prot
+
+    return dict_proteins_error
+
+def checkWholeDna(whole_dna):
+    """
+    check if the sequence exists for a whwhole_dna
+
+    :return: dictionary with the id and sequences if they are to short or inexistent
+    :rtype: dict[id_whole_dna]:sequence
+    """
+    dict_whole_dna_error = {}
+    if len(whole_dna.sequence_DNA) < 1000:
+        dict_whole_dna_error[whole_dna.id] = whole_dna.sequence_DNA
+    return dict_whole_dna_error
+
+def checkSequenceContigs(list_contigs):
+    """
+    check if the sequence exists for a contig given a list of them
+
+    :return: dictionary with the ids and sequences if they are to short or inexistent
+    :rtype: dict[id_contig]:sequence
+    """
+    dict_contigs_error = {}
+    for contig in list_contigs:
+        if len(contig.sequence_DNA) < 15:
+            print('error')
+            id_contig = contig.id
+            contig_sequence = contig.sequence_DNA
+            dict_contigs_error[id_contig] = contig_sequence
+
+    return dict_contigs_error
+
 def getAllCouples():
     list_couple = CoupleJson.getAllAPI()
     return list_couple
